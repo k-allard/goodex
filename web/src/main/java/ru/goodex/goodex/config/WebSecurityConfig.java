8d128b246c3267aa2d166f278d36a67d18381252
Gerch17 <tambovtsevy228@mail.ru>
2021-07-12 14:50:33 +0300

    GS-27 add registration

---
+++ web/src/main/java/ru/goodex/goodex/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+package ru.goodex.goodex.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/home").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .permitAll();
+
+    }
+}
